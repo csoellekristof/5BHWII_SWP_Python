cb50c4b8aa49e1e9d5d5251aba620d852f5412ba
csoellekristof <90830893+csoellekristof@users.noreply.github.com>
2022-09-23 08:27:57 +0200

    cesar verschlüsselung für NWES

---
+++ Cesar.py
@@ -0,0 +1,9 @@
+word = "django"
+
+code = ""
+
+for c in word:
+    #code += chr(ord(c+3))
+    print(chr(ord(c)+3))
+
+#print (code)
--- Lotto.py
+++ Lotto.py
@@ -15,7 +15,6 @@ for i in range(1000):
     for i in range (6):
         rand_num = random.choice(lottolist)
         lottodict[rand_num] = lottodict[rand_num] + 1
-        print(rand_num)
         lottolist.remove(rand_num)
 
 print (lottodict)
