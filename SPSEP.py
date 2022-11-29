69700f23e332b1250298408695de9b898ea274ff
csoellekristof <90830893+csoellekristof@users.noreply.github.com>
2022-11-29 21:12:35 +0100

    saving data in file as json

--- SPSEP.py
+++ SPSEP.py
@@ -1,10 +1,19 @@
 import random
+import json
 
 
 hand_choices = ["Schere", "Papier", "Stein", "Echse", "Spock"]
 
 def cp_pick_hand():
     return hand_choices[random.randrange(0,4)]
+
+def read_userpicks():
+    with open("userpicks.txt", "r") as f:
+        return json.loads(f.read())
+
+def write_userpicks(userpicks):
+    with open("userpicks.txt", "w") as f:
+        f.write(json.dumps(userpicks))
 
 
 def check_user_win(user_hand, cp_hand):
@@ -15,16 +24,26 @@ def check_user_win(user_hand, cp_hand):
 
 
 if __name__ == "__main__":
+    userpicks = read_userpicks()
+    user = input("Gebe deine Name ein:")
     while(True):
-        user = input("Gebe deinen Namen ein")
         user_hand = input("Wähle deinen Hand: Schere, Papier, Stein, Echse, Spock")
         cp_hand = cp_pick_hand()
+        if user in userpicks.keys():
+            userpicks[user][user_hand] = userpicks[user][user_hand] + 1
+        else:
+            userpicks[user] = {"Gewonnen": 0, "Schere":0, "Papier":0, "Stein":0, "Echse":0, "Spock":0}
+            userpicks[user][user_hand] = 1
+
         if user_hand == cp_hand:
             print("Der Computer hat:" + cp_hand + " gewählt")
             print("Unentschieden")
-        if check_user_win(user_hand, cp_hand):
+        elif check_user_win(user_hand, cp_hand):
+            userpicks[user]["Gewonnen"] += 1
             print("Der Computer hat:" + cp_hand + " gewählt")
             print("Du hast gewonnen!")
         else:
             print("Der Computer hat " + cp_hand + " gewählt")
             print("Du hast leider verloren! :(")
+
+        write_userpicks(userpicks)
